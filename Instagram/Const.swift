d6c3fa8c577adb53082e1907f68d349ae1c79f1c
天野智広 <amanotomohiro@amanotomohiro-no-macbook-pro.local>
2019-03-30 06:18:40 +0900

    課題実施前

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 天野智広 on 2019/03/16.
+//  Copyright © 2019 天野智広. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
